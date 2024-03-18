135f611096fa03b29688eeabbea880e92bdcd57d
jjunnong <cacao3216@gmail.com>
2024-03-18 11:37:38 +0900

    flutter_ss03

--- chap03_flutter_sns/lib/main.dart
+++ chap03_flutter_sns/lib/main.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import 'feed.dart';
+import 'home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,9 +13,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(),
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
   }
 }
