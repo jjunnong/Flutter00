2545c5df162129f7c5dc426684825e726313771d
jjunnong <cacao3216@gmail.com>
2024-03-18 15:24:35 +0900

    commit

--- chap05_flutter_provider_to_do_list/lib/main.dart
+++ chap05_flutter_provider_to_do_list/lib/main.dart
@@ -1,10 +1,15 @@
+import 'package:chap05_flutter_provider_to_do_list/to_do_service.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(
-    const MyApp(),
-  );
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(create: (context) => ToDoService()),
+    ],
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -19,37 +24,78 @@ class MyApp extends StatelessWidget {
   }
 }
 
-/// ToDo 클래스
-class ToDo {
-  String job; // 할 일
-  bool isDone; // 완료 여부
-
-  ToDo(this.job, this.isDone); // 생성자
-}
-
 /// 홈 페이지
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("ToDo 리스트"),
-      ),
-      body: Center(child: Text("ToDo 리스트를 작성해 주세요.")),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          // + 버튼 클릭시 ToDo 생성 페이지로 이동
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (_) => CreatePage()),
-          );
-        },
-      ),
+    return Consumer<ToDoService>(
+      builder: (context, toDoService, child) {
+        // toDoService로 부터 toDoList 가져오기
+        List<ToDo> toDoList = toDoService.toDoList;
+
+        return Scaffold(
+          appBar: AppBar(
+            title: Text("ToDo 리스트"),
+          ),
+          body: toDoList.isEmpty
+              ? Center(
+                  child: Text("To Do List를 작성해주세요"),
+                )
+              : ListView.builder(
+                  itemCount: toDoList.length,
+                  itemBuilder: (context, index) {
+                    ToDo toDo = toDoList[index];
+                    return ListTile(
+                      title: Text(
+                        toDo.job,
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: toDo.isDone ? Colors.grey : Colors.black,
+                          decoration: toDo.isDone
+                              ? TextDecoration.lineThrough
+                              : TextDecoration.none,
+                        ),
+                      ),
+                      trailing: IconButton(
+                        icon: Icon(
+                          CupertinoIcons.delete,
+                        ),
+                        onPressed: () {
+                          //삭제 버튼이 눌렀을 때 작동
+                          toDoService.deleteToDo(index);
+                        },
+                      ),
+                      onTap: () {
+                        //아이템 클릭시
+                        toDo.isDone = !toDo.isDone;
+                        toDoService.updateToDo(toDo, index);
+                      },
+                    );
+                  }),
+          floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: () {
+              // + 버튼 클릭시 ToDo 생성 페이지로 이동
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (_) => CreatePage()),
+              );
+            },
+          ),
+        );
+      },
     );
   }
+}
+
+/// ToDo 클래스
+class ToDo {
+  String job; // 할 일
+  bool isDone; // 완료 여부
+
+  ToDo(this.job, this.isDone); // 생성자
 }
 
 /// ToDo 생성 페이지
@@ -118,6 +164,10 @@ class _CreatePageState extends State<CreatePage> {
                       // 내용이 있는 경우 에러 메세지 숨김
                       error = null;
                     });
+                    //ToDOService 인스턴스 생성
+                    ToDoService toDoService = context.read<ToDoService>();
+                    // ToDoService에 있는 createToDo 메서드 사용
+                    toDoService.createToDo(job);
                     Navigator.pop(context);
                   }
                 },
